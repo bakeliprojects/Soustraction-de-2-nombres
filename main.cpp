6707dd7060ca9a1f6a833fd8d2776cdcbb6220d3
tawfeh <sennegueye@gmail.com>
2017-08-07 09:39:45 +0000

    Add All files

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+int a,b,c ;
+
+    cout << "saisir un nombre " << endl;
+     cin >> a;
+     cout << "saisir un autre nombre " << endl;
+     cin >> b;
+
+     c= a-b;
+    cout << a << " - " << b << " = "<< c <<endl;
+       return 0;
+
+}
+
+
